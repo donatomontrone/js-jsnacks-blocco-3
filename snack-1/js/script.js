2aba8ec9fbdbd850f9d0aa20288d9199989caa10
donatomontrone <donato.montrone@outlook.com>
2022-11-28 16:28:30 +0100

    added do while condition

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -1,10 +1,19 @@
 // Continuare a chiedere all'utente un numero tra 1 e 10 fino a quando l'utente non inserisce il valore corretto
 
 
-let userNumber = parseInt(prompt('Inserisci un numero tra 1 e 10'));
+// let userNumber = parseInt(prompt('Inserisci un numero tra 1 e 10'));
 
-while ((Number.isNaN(userNumber) || userNumber == 0 || userNumber > 10)) {
+// while ((Number.isNaN(userNumber) || userNumber == 0 || userNumber > 10)) {
+//     userNumber = parseInt(prompt('Inserisci un numero tra 1 e 10'));
+// }
+
+// console.log(`Hai inserito il numero ${userNumber}`)
+
+
+let userNumber;
+
+do {
     userNumber = parseInt(prompt('Inserisci un numero tra 1 e 10'));
-}
+} while (((Number.isNaN(userNumber) || userNumber == 0 || userNumber > 10)));
 
 console.log(`Hai inserito il numero ${userNumber}`)
