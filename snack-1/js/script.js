e1d194b13d8129c17511daf2376016a789784140
donatomontrone <donato.montrone@outlook.com>
2022-11-28 16:26:12 +0100

    added

---
+++ snack-1/js/script.js
@@ -0,0 +1,10 @@
+// Continuare a chiedere all'utente un numero tra 1 e 10 fino a quando l'utente non inserisce il valore corretto
+
+
+let userNumber = parseInt(prompt('Inserisci un numero tra 1 e 10'));
+
+while ((Number.isNaN(userNumber) || userNumber == 0 || userNumber > 10)) {
+    userNumber = parseInt(prompt('Inserisci un numero tra 1 e 10'));
+}
+
+console.log(`Hai inserito il numero ${userNumber}`)
